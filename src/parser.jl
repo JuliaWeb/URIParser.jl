d0b4bec11aeb83a26d174888221ae94204f7cbc7
Stefan Karpinski <stefan@karpinski.org>
2016-05-15 18:12:45 -0400

    Compat update for String rename

--- src/parser.jl
+++ src/parser.jl
@@ -9,13 +9,13 @@ is_host_char(c) = isalnum(c) || (c == '.') || (c == '-') || (c == '_') || (c == 
 
 
 immutable URI
-    scheme::Compat.ASCIIString
-    host::Compat.ASCIIString
+    scheme::String
+    host::String
     port::UInt16
-    path::Compat.ASCIIString
-    query::Compat.ASCIIString
-    fragment::Compat.ASCIIString
-    userinfo::Compat.ASCIIString
+    path::String
+    query::String
+    fragment::String
+    userinfo::String
     specifies_authority::Bool
     URI(scheme,host,port,path,query="",fragment="",userinfo="",specifies_authority=false) =
             new(scheme,host,UInt16(port),path,query,fragment,userinfo,specifies_authority)
