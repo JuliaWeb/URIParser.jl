b7d7d6f68da6aba25fd20e9e7b55105da2649c56
rofinn <rory.finnegan@gmail.com>
2017-07-21 00:17:13 -0500

    Fixed Compat deprecation warnings.
    - Converted Compat.ASCIIString to String.
    - Bumped julia minimum version number as Compat has dropped support for 0.4.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,13 +1,13 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(URIParser.parse_url, (Compat.ASCIIString,))
-    precompile(URIParser.parse_authority, (Compat.ASCIIString, Bool,))
-    precompile(URIParser.escape_with, (Compat.ASCIIString, Compat.String,))
+    precompile(URIParser.parse_url, (String,))
+    precompile(URIParser.parse_authority, (String, Bool,))
+    precompile(URIParser.escape_with, (String, Compat.String,))
     precompile(URIParser.is_host_char, (Char,))
     precompile(URIParser.is_url_char, (Char,))
     precompile(URIParser.isnum, (Char,))
     precompile(URIParser.is_mark, (Char,))
     precompile(URIParser.is_userinfo_char, (Char,))
-    precompile(URIParser.escape, (Compat.ASCIIString,))
+    precompile(URIParser.escape, (String,))
     precompile(URIParser.ishex, (Char,))
 end
